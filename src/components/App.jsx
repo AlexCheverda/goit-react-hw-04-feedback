cc229485a5a41fb968e287381fb6cd2bfa29d76d
Aleksandr Cheverda <alexar@ua.fm>
2022-11-19 17:45:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Section from "./Section/Section";
 import Notification from "./Notification/Notification";
 import Statistics from "./Statistics/Statistics";
 import Feedback from "./Feedback/Feedback";
 import style from './App.module.css'
 
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onFeedbackBtnClick = option => {
+    switch (option) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleIncrement = option => {
-    this.setState(prevState => {
-      return { [option]: prevState[option] + 1 };
-    });
+
+  // handleIncrement = option => {
+  //   this.setState(prevState => {
+  //     return { [option]: prevState[option] + 1 };
+  //   });
+  // };
+
+  const countTotalFeedback = () => {
+    // const totalValue = Object.values(this.state);
+    return good + neutral + bad;
   };
 
-  countTotalFeedback() {
-    const totalValue = Object.values(this.state);
-    return totalValue.reduce((acc, value) => acc + value);
-  }
+  const countPositiveFeedbackPercentage = () => {
+    const positivePercentage = Number(
+      ((good / countTotalFeedback()) * 100).toFixed(0)
+    );
+    // const value = this.countTotalFeedback();
+    // const percentage = Math.round((this.state.good / value) * 100);
 
-  countPositiveFeedbackPercentage() {
-    const value = this.countTotalFeedback();
-    const percentage = Math.round((this.state.good / value) * 100);
-
-    if (!percentage) return 0;
-    return percentage;
-  }
+    // if (!percentage) return 0;
+    return positivePercentage;
+  };
 
   render() {
     const { good, neutral, bad } = this.state;
